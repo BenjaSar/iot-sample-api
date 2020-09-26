152ee3d3fcac6a67d7095cd1730f9b59c697c48f
Gustavo Ramoscelli <gustavoramoscelli@gmail.com>
2020-09-26 02:48:17 -0300

    add Vue compnent vue-mqtt

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Client;
+use Illuminate\Http\Request;
+
+class ClientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $clients = Client::all();
+    
+        return $this->sendResponse(ClientResource::collection($clients), 'Clients retrieved successfully.');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+   
+        $validator = Validator::make($input, [
+            'name' => 'required',
+            'client_id' => 'required'
+        ]);
+   
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());       
+        }
+   
+        $client = Client::create($input);
+   
+        return $this->sendResponse(new ClientResource($product), 'Client created successfully.');
+  
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Client  $client
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Client $client)
+    {
+        $product = Client::find($id);
+  
+        if (is_null($product)) {
+            return $this->sendError('Client not found.');
+        }
+   
+        return $this->sendResponse(new ClientResource($product), 'Client retrieved successfully.');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Client  $client
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Client $client)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Client  $client
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Client $client)
+    {
+        $input = $request->all();
+   
+        $validator = Validator::make($input, [
+            'name' => 'required',
+            'detail' => 'required'
+        ]);
+   
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());       
+        }
+   
+        $product->name = $input['name'];
+        $product->detail = $input['detail'];
+        $product->save();
+   
+        return $this->sendResponse(new ClientResource($product), 'Client updated successfully.');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Client  $client
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Client $client)
+    {
+        $product->delete();
+   
+        return $this->sendResponse([], 'Client deleted successfully.');
+    }
+}
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
 require('./bootstrap');
+
+import Vue from 'vue'
+import VueMqtt from 'vue-mqtt'
+import MqttExample from './components/MqttExample.vue';
+
+Vue.use(VueMqtt, 'ws://localhost:9001', {clientId: 'WebClient-' + parseInt(Math.random() * 100000)})
+Vue.config.productionTip = false
+Vue.config.productionTip = false
+Vue.component('mqtt-example',  MqttExample);
+
+const app = new Vue({
+    el: '#app'
+});
+
